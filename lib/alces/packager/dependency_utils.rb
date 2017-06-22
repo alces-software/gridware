a315f3a24260c37b514eff1f4113577f4b08dc70
James Muscat <james.muscat@alces-software.com>
2017-06-22 16:20:34 +0100

    Strip `_variant` from package path.

--- lib/alces/packager/dependency_utils.rb
+++ lib/alces/packager/dependency_utils.rb
@@ -6,7 +6,16 @@ module Alces
         def generate_dependency_script(package_path, phase)
           %(#=Alces-Gridware-Dependencies:2
 cw_GRIDWARE_userspace=#{ENV['cw_GRIDWARE_userspace']}
-sudo -E #{ENV['cw_ROOT']}/bin/alces gridware distro_deps #{package_path} --phase #{phase})
+sudo -E #{ENV['cw_ROOT']}/bin/alces gridware distro_deps #{strip_variant(package_path)} --phase #{phase})
+        end
+
+        private
+
+        def strip_variant(package_path)
+          # In the second-to-last component of the path (e.g. allow _ in version)
+          # remove _ and everything subsequent up until the next /
+          # This assumes we always get a full package path including version...
+          package_path.gsub(/_[^\/]+(\/[^\/]+)$/, '\1')
         end
 
       end
