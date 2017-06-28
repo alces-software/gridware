d0bf14db15e5ab5f847e3b40a368cd6e14989d12
James Muscat <james.muscat@alces-software.com>
2017-06-28 14:21:56 +0100

    Set environment variable properly in dep script.

--- lib/alces/packager/dependency_utils.rb
+++ lib/alces/packager/dependency_utils.rb
@@ -5,8 +5,7 @@ module Alces
 
         def generate_dependency_script(package_path, phase)
           %(#=Alces-Gridware-Dependencies:2
-cw_GRIDWARE_userspace=#{ENV['cw_GRIDWARE_userspace']}
-sudo -E #{ENV['cw_ROOT']}/bin/alces gridware distro_deps #{strip_variant(package_path)} --phase #{phase})
+sudo -E cw_GRIDWARE_userspace=#{ENV['cw_GRIDWARE_userspace']} #{ENV['cw_ROOT']}/bin/alces gridware distro_deps #{strip_variant(package_path)} --phase #{phase})
         end
 
         private
